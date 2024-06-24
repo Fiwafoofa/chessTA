2b656580cf0f77019e7b73b40d7b6a67c8c4b5e5
Isaih Barron <isaihbarron@gmail.com>
2024-06-24 09:44:48 -0600

    add UserService Tests so I pass commit requirement 2

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,13 +1,18 @@
 package service;
 
+import dataaccess.DAOFactory;
+import dataaccess.memory.MemoryDAOFactory;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClearServiceTest {
 
+  DAOFactory memoryDaoFactory = new MemoryDAOFactory();
+  ClearService clearService = new ClearService(memoryDaoFactory);
+
   @Test
   void clear() {
-    assertTrue(true);
+    assertDoesNotThrow(() -> clearService.clear());
   }
 }
